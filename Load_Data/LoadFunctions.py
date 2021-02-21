07dd25c3455c9f2d8c3b215ead3acef7b3ff286f
laiwenghong <73129702+laiwenghong@users.noreply.github.com>
2021-02-22 02:55:45 +0800

    Update LoadFunctions.py

--- Load_Data/LoadFunctions.py
+++ Load_Data/LoadFunctions.py
@@ -82,23 +82,32 @@ def modeChanged(data):
       totalNumberOfModeChanged += 1
       duration = data['Cleaned_Time'][i] - timeStampLastMode
       timestampList[-1][2] = duration #only after next change of mode we are able to calculate the duration of last mode, without this the mode and duration output will be interchanged
+      
+      #insert end time for previous mode as current timestamp
+      timestampList[-1].append(data['Time'][i])
+
+      #insert new entries
       timestampList.append([mode,data['Time'][i],0]) #0 is just preparation for adding duration in the next loop
 
       #update new count
       current_mode = mode
       timeStampLastMode = data['Cleaned_Time'][i]
-  print("Total number of Mode changed is :",totalNumberOfModeChanged)
-  return totalNumberOfModeChanged, timestampList
+#  print("Total number of Mode changed is :",totalNumberOfModeChanged)
+  return timestampList
 
 def findErrorDuration(timestampList):
   errorCount = 0
+  global errorList
+  errorList = []
   for item in timestampList:
     if item[2] < 20:
       errorCount+=1
-      print([item[1],item[2]])
+      errorList.append([item[1],item[2]])
   print("Total number of error entries is :",errorCount)
-  return errorCount
+  print(errorList)
+  return errorCount, errorList
 
+#not useful anymore
 def findErrorDuration2(timestampList):
   errorCount = 0
   for item in timestampList:
@@ -107,3 +116,144 @@ def findErrorDuration2(timestampList):
       print([item[1],item[2]])
   print("Total number of error entries is :",errorCount)
   return errorCount
+
+def removeBackwardTimestampError(modeList):
+  newModeList = [modeList[0]]
+  removeList = []
+  currentTime = modeList[0][1]
+  modeList.pop(0)
+  while modeList != []:
+    if modeList[0][1] <= currentTime:
+      removeList.append(modeList[0])
+      modeList.remove(modeList[0])
+    else:
+      newModeList.append(modeList[0])
+      currentTime = modeList[0][1]
+      newModeList[-2][3] = currentTime
+      modeList.remove(modeList[0])
+  return newModeList
+
+def recalculateDuration(modelist):
+  for i in range(len(modelist)-1):
+    if isinstance(modelist[0][1],str) == True:
+      endtime = datetime.datetime.strptime(modelist[i][3], '%Y-%m-%d %H:%M:%S')
+      starttime = datetime.datetime.strptime(modelist[i][1], '%Y-%m-%d %H:%M:%S')
+    else:
+      endtime = modelist[i][3]
+      starttime = modelist[i][1]
+    time = endtime - starttime
+    modelist[i][2] = time.total_seconds()
+  return modelist
+
+def removeErrorDuration(modelist):
+  newlist = []
+  for i in range(len(modelist)-1):
+    if modelist[i][2] < 5: #less than 5 seconds
+      pass
+    else:
+      newlist.append(modelist[i])
+  newlist.append(modelist[-1]) #add last item in
+  return newlist
+
+def selectTimestampKey(modelist):
+  newlist = []
+  for i in modelist:
+    newlist.append(i[1])
+  return newlist
+
+def findRepetitiveMode(modelist):
+  repetitivelist = []
+  mode = modelist[0][0]
+  for i in range(1,len(modelist)):
+    if modelist[i][0] == mode:
+      print("Repetitve mode error at ",modelist[i])
+      repetitivelist.append(modelist[i])
+    else:
+      mode = modelist[i][0]
+  return repetitivelist
+
+def removeRepetitiveMode(modelist, repetitivelist):
+  newlist = []
+  print("remove starting here!")
+  for i in range(len(modelist)-1):
+    if modelist[i] in repetitivelist:
+      print(modelist[i],"is going to be removed")
+      newlist[-1][3] = modelist[i][3]
+    else:
+      newlist.append(modelist[i])
+#  print("newlist length is:", len(newlist))
+  return newlist
+
+def checkTimestampIsString(modelist):
+  newlist = []
+  if isinstance(modelist[0][1], str) == False:
+    for i in range(len(modelist)-1):
+      modelist[i][1] = str(modelist[i][1])
+      modelist[i][3] = str(modelist[i][3])
+      newlist.append(modelist[i])
+    newlist.append(modelist[-1])
+    newlist[-1][1] = str(newlist[-1][1])
+    return newlist
+  return modelist
+
+def errorRemovingPipeline(data):
+  modeChangedList = modeChanged(data)
+  print("The length after modeChanged is :", len(modeChangedList))
+
+  newModeChangedList = removeBackwardTimestampError(modeChangedList)
+  print("The length after removeBackwardTimestampError is :", len(newModeChangedList))
+
+  recalculatedModeList = recalculateDuration(newModeChangedList)
+  print("The length after first recalculated is :", len(recalculatedModeList))
+
+  recalculatedAndRemovedList = removeErrorDuration(recalculatedModeList)
+
+  repetitiveList = findRepetitiveMode(recalculatedAndRemovedList)
+  if repetitiveList != []:
+    recalculatedAndRemovedList = removeRepetitiveMode(recalculatedAndRemovedList, repetitiveList)
+
+  print("The length after recalculated and repetitive mode removed is :", len(recalculatedAndRemovedList))
+  finalList = checkTimestampIsString(recalculatedAndRemovedList)
+
+  finalList[-1].append('0')
+
+  return finalList
+
+def appendStation(modelist,stationlist):
+  tempStationList = stationlist[:] #copy without referencing, if not stationlist will get pop and cannot be reused
+  for item in modelist:
+    if item[0] == "Idle" or item[0] == "PMD": #put PMD because TE line got PMD as Idle
+      print(tempStationList[0])
+      item.append(tempStationList[0])
+      tempStationList.pop(0)
+    else:
+      item.append("Moving")
+  return modelist
+
+def addStationToDf(data,modelist):
+  data['Station'] = '0'
+  while modelist != []:
+    for i in range(len(data['Time'])):
+      if str(data['Time'][i]) == modelist[0][1]:
+        data['Station'][i] = modelist[0][4]
+        print(data['Station'][i],'is added!!!')
+        modelist.pop(0)
+        break
+  return data
+
+def fillEmptyStationToDf(data):
+  temp = data['Station'][0]
+  for i in range(len(data['Station'])):
+    if data['Station'][i] != '0':
+      temp = data['Station'][i]
+    else:
+      data['Station'][i] = temp
+  return data
+
+#combine 2 functions together
+def modifyColumnStation(data,modelist):
+  data1 = addStationToDf(data,modelist)
+  print("add station done!!!")
+  data2 = fillEmptyStationToDf(data1)
+  print("fill empty done!!!")
+  return data2
