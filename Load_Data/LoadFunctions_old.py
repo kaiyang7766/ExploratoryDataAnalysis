1814b74e9f668511edb77dadd3f4ebb9f4db56d3
Kai Yang <65525083+kaiyang7766@users.noreply.github.com>
2021-04-13 23:58:16 +0800

    Update LoadFunctions_old.py

--- Load_Data/LoadFunctions_old.py
+++ Load_Data/LoadFunctions_old.py
@@ -118,12 +118,12 @@ def removeBackwardTimestampError(modeList):
       modeList.remove(modeList[0])
   return newModeList
 
-import datetime
+#import datetime
 def recalculateDuration(modelist):
   for i in range(len(modelist)-1):
     if isinstance(modelist[0][1],str) == True:
-      endtime = datetime.datetime.strptime(modelist[i][3], '%Y-%m-%d %H:%M:%S')
-      starttime = datetime.datetime.strptime(modelist[i][1], '%Y-%m-%d %H:%M:%S')
+      endtime = datetime.strptime(modelist[i][3], '%Y-%m-%d %H:%M:%S')
+      starttime = datetime.strptime(modelist[i][1], '%Y-%m-%d %H:%M:%S')
     else:
       endtime = modelist[i][3]
       starttime = modelist[i][1]
